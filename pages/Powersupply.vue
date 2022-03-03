2111d286276b2ba82b50771a17edacb0c27a03f8
Asieef <asieef.chowdhury@gmail.com>
2022-03-03 11:53:23 +0600

    favicon

--- pages/Powersupply.vue
+++ pages/Powersupply.vue
@@ -28,6 +28,16 @@
 // import VueTailwindPagination from "@ocrv/vue-tailwind-pagination";
 
 export default {
+  head: {
+    title: "Value-Top | Power Supply",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+  },
+
   data() {
     return {
       products: [],
